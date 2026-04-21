9a58c3dcfdba0776b5e792c03fe7735f59757bb2
devsdaddy <start@ncommx.com>
2026-04-21 20:10:19 +0500

    Add parsers

--- src/shared/utils/parse.ts
+++ src/shared/utils/parse.ts
@@ -3,10 +3,10 @@
  *
  * @author                Elijah Rastorguev
  * @version               1.0.0
- * @build                 1000
+ * @build                 1001
  * @git                   https://github.com/devsdaddy/bitwarp
  * @license               MIT
- * @updated               10.04.2026
+ * @updated               21.04.2026
  */
 import { FormatUtils } from './format';
 
@@ -23,4 +23,23 @@ export class ParseUtils {
     if(typeof bool === "string") return (bool.toLowerCase() === "true");
     return FormatUtils.range(bool, 0, 1) === 1;
   }
+
+  /**
+   * Parse BigIng
+   * @param input {number | boolean | bigint | string} Input Number
+   * @returns {BigInt} Output BigInt
+   */
+  public static bigInt(input : number | boolean | bigint | string) : BigInt {
+    return BigInt(input);
+  }
+
+  /**
+   * Parse number
+   * @param input {any} Input number
+   * @returns {number} Output converted number (float64)
+   */
+  public static number(input : any) : number{
+    return Number(input);
+  }
+
 }
